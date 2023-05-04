eca78b1cfda78c3489c50555a80485519c83cea2
Koushik Sen <ksenx@google.com>
2023-05-04 19:19:37 +0000

    string_add_analysis

---
+++ demos/string_add.py
@@ -0,0 +1,7 @@
+
+
+def ret_str():
+  return "Hello World"
+
+x = "prefix " + ret_str() + " suffix"
+print(x)
---
+++ pynsy/analyses/string_add_analysis.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+log_file = f"trace_compact.csv"
+
+history = []
+
+def is_blank(val):
+  return val != val
+
+def abstraction(obj):
+  if isinstance(obj, str):
+    return False, obj
+  else:
+    return True, None
+
+def process_event(record):
+  if record["type"] == "BINARY_ADD":
+    if str(record["result_and_args"][0]["type"]) == "<class 'str'>":
+      print(f"Warning: at line {record['lineno']} in {record['module_name']}, the 'string addition' is slow.  Use \"\".join(str1, str2)")
+  return record
+
+def process_termination(record_list):
+  df = pd.DataFrame(record_list)
+  #df = df.explode('result_and_args')
+  print("Saving raw data as a pandas Dataframe in " + log_file)
+  pd.DataFrame.to_csv(df, log_file)
