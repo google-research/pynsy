94aa29306bc441eba460a81c09f516d632bcd35c
Dan Zheng <danielzheng@google.com>
2023-06-10 19:13:22 -0700

    Update demos.

--- demos/key_in_list.py
+++ demos/key_in_list.py
@@ -1,8 +1,22 @@
+# Copyright 2023 The pynsy Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 d = range(10000)
 e = range(50)
 queries = [-1, 9, 100000, -100000]
 for query in queries:
   if query in d:  # Slow
-    print(f'Found {query}')
+    print(f"Found {query}")
   if query in e:
-    print(f'Found {query}')
+    print(f"Found {query}")
--- demos/mnist.py
+++ demos/mnist.py
@@ -1,3 +1,17 @@
+# Copyright 2023 The pynsy Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,13 +34,13 @@ The primary aim here is simplicity and minimal dependencies.
 
 import time
 
+from jax import grad
+import jax.numpy as jnp
+from jax.scipy.special import logsumexp
 import numpy.random as npr
 
-from jax import grad
-from jax.scipy.special import logsumexp
-import jax.numpy as jnp
-import demos.datasets as datasets
 import pynsy.analyses.shape_logger as shaper
+import demos.datasets as datasets
 
 
 def init_random_params(scale, layer_sizes, rng=npr.RandomState(0)):
